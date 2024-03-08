f77a3e79e8dab4b014df218807ab5a33cc0e1250
omrzcn <omrzcnn@gmail.com>
2024-03-08 10:55:14 +0300

    Algo Task

---
+++ NumOfPeopleInTheBus.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class NumOfPeopleInTheBus {
+
+    public static void main(String[] args) {
+        ArrayList<int[]> busStops = new ArrayList<int[]>();
+        busStops.add(new int[] {10,0});// first stop
+        busStops.add(new int[] {3,5});
+        busStops.add(new int[] {2,5}); // last stop
+        System.out.println(countPassengers(busStops));
+    }
+    public static int countPassengers(ArrayList<int[]> stops) {
+        // Runs in O(n) complexity because there is a single loop with size n
+        int numOfPeople=0;
+        for (int i = 0; i < stops.size(); i++) {
+            numOfPeople=numOfPeople+ stops.get(i)[0] - stops.get(i)[1];
+        }
+        return numOfPeople;
+    }
+}
+/*
+There is a bus moving in the city which takes and drops some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+
+ */

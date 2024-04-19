8990d9fcb59202241d3532176da981ce3eada5d8
omrzcn <omrzcnn@gmail.com>
2024-04-19 17:14:55 +0300

    node created

---
+++ LinkedList/src/main/java/com/cydeo/Node.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class Node {
+    int id;
+
+    Node next;   // i will store adress of the next element. With this variable, i refer nex=t element.
+
+    public Node(int id) { // i created constructor to use this Node in other class.
+        this.id = id;
+    }
+
+}
